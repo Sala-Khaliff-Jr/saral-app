1db570ae9a581363ff2fb9d109599db3555138be
Sala-Khaliff-Jr <salakhaliff@gmail.com>
2020-02-24 19:56:34 +0530

    register id added

--- register/models.py
+++ register/models.py
@@ -5,3 +5,6 @@ class Registrations(models.Model):
     student_name = models.CharField(blank=False,null=False,max_length=30)
     college_name = models.CharField(blank=False,null=False,max_length=50)
     email_id = models.EmailField(unique=True)
+    reg_id = models.CharField(unique=True,max_length=16)
+    def __str__(self):
+        return self.student_name
--- register/views.py
+++ register/views.py
@@ -2,25 +2,31 @@ from django.shortcuts import render
 from .models import Registrations
 # Create your views here.
 def register_view(request):
-    if 'email' in request.session:
-        student_name = request.session['student_name']
-        return(render(request,'register.html',{'submit':'ALREADY','student_name':student_name }))
     if(request.method == "GET"):
-        return(render(request,'register.html',{'submit':'NO'}))
+        if 'email' in request.session:
+            student_name = request.session['student_name']
+            return(render(request,'register.html',{'submit':'ALREADY','student_name':student_name }))
+        else:
+            return(render(request,'register.html',{'submit':'NO'}))
     if(request.method=="POST"):
         # print(request.POST.get('name'))
         # print(request.POST.get('college'))
-        try:
-            student_name = request.POST.get('name')
-            email_id = request.POST.get('email')
-            college_name = request.POST.get('college')
-            request.session['student_name'] = student_name
-            request.session['email'] = email_id
-            Registrations.objects.create(student_name=student_name,email_id=email_id,college_name=college_name)
-            reg_id = Registrations.objects.get(email_id=email_id)
-            return(render(request,'register.html',{'submit':'Yes','student_name':student_name,'id':reg_id.id}))
-        except:
-            return(render(request,'register.html',{'submit':'NO'}))
+        # try:
+        student_name = request.POST.get('name')
+        email_id = request.POST.get('email')
+        college_name = request.POST.get('college')
+        
+        Registrations.objects.create(student_name=student_name,email_id=email_id,college_name=college_name,reg_id="")
+        register = Registrations.objects.get(email_id=email_id)
+        print(Registrations.objects.filter(email_id=email_id))
+        Registrations.objects.filter(email_id=email_id).update(reg_id="SARAL00"+str(register.id))
+        
+        request.session['student_name'] = student_name
+        request.session['email'] = email_id
+
+        return(render(request,'register.html',{'submit':'Yes','student_name':student_name,'id':register.reg_id}))
+        # except:
+        #     return(render(request,'register.html',{'submit':'NO'}))
 def view_registrations(request):
     registrations = Registrations.objects.all()
     return render(request,'viewreg.html',{'registrations':registrations})
