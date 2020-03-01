2f8777993cffa3331a13b2621cccfae658940bab
Sala-Khaliff-Jr <salakhaliff@gmail.com>
2020-03-01 12:04:29 +0000

    server

--- saral_app/settings.py
+++ saral_app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'zq+v6n2*z4f$(ix_vgly@2e-u05kqf8m%#f7nsf!=-ec5#286_'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['saaralkite.pythonanywhere.com']
 
 
 # Application definition
